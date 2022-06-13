692b78cfdc7f173690d912434e4da995771546ec
Eiekama <42797046+Eiekama@users.noreply.github.com>
2022-06-13 23:15:38 +0800

    improved ui visuals
    and also swapped to female assassin model. even if its mesh is messed up

---
+++ Assets/Scripts/MenuAnimationBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimationBehaviour : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("Look_b", false);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.normalizedTime > 4)
+        {
+            animator.SetBool("Look_b", true);
+        }
+    }
+}
